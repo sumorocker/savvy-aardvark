02326afc85bab6b67cb80ba3e64eb47912531c6a
sumorocker <macdonald.allan@live.com>
2016-01-14 16:47:41 -0500

    compare and history added to our fucking goliath

--- cloud-parse/cloud/main.js
+++ cloud-parse/cloud/main.js
@@ -2,7 +2,7 @@ Parse.Cloud.define("UPC", function(request, response) {
 
 
 Parse.Cloud.httpRequest({
-  url: 'http://api.foodessentials.com/labelarray?u='+request.params.search+'&sid=1cf87436-1f47-4fc6-99c1-8513d5ab97c7&n=10&s=0&f=json&api_key=3dqu23v7j8v4ey5wu4cvcxcj',
+  url: 'http://api.foodessentials.com/labelarray?u='+request.params.search+'&sid=1cf87436-1f47-4fc6-99c1-8513d5ab97c7&n=100&s=0&f=json&api_key=3dqu23v7j8v4ey5wu4cvcxcj',
 }).then(function(httpResponse) {
 	console.log(httpResponse);
 	response.success(JSON.parse(httpResponse.text));
@@ -18,8 +18,10 @@ Parse.Cloud.httpRequest({
 
 Parse.Cloud.define("productName", function(request, response) {
 
+var APIprod =   'http://api.foodessentials.com/searchprods?q='+request.params.search+'&sid=a4c7009c-f312-4239-a63a-1ee7d55e9046&n=100&s=0&f=json&api_key=3dqu23v7j8v4ey5wu4cvcxcj'
+
 Parse.Cloud.httpRequest({
-  url: 'http://api.foodessentials.com/searchprods?q='+request.params.search+'&sid=a4c7009c-f312-4239-a63a-1ee7d55e9046&n=10&s=0&f=json&api_key=3dqu23v7j8v4ey5wu4cvcxcj'
+    url: APIprod
 }).then(function(httpResponse) {
 	console.log(httpResponse);
 	response.success(JSON.parse(httpResponse.text)); 
